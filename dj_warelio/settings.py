230226368bc14df343005d3f43b19aa8620a66d1
ghazalmohseni7 <ghazalmohseni79@gmail.com>
2025-05-15 22:01:15 +0330

    feat: add drf spectacular

--- dj_warelio/settings.py
+++ dj_warelio/settings.py
@@ -124,3 +124,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# swagger drf-spectacular
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Warelio',
+    'DESCRIPTION': '',
+    'VERSION': '0.1.0',
+    'COMPONENT_SPLIT_REQUEST': True  # this is for showing multipart
+}
+# rest frame work settings
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
--- dj_warelio/urls.py
+++ dj_warelio/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/', SpectacularSwaggerView.as_view(), name='swagger'),
     path('', include('project.urls')),
 ]
