516e4be1b711946583518740fe0039c2c44f8dab
ghazalmohseni7 <ghazalmohseni79@gmail.com>
2025-05-18 16:59:03 +0330

    fix: stock entry serializer validation

--- stock_entry/serializers.py
+++ stock_entry/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from stock_entry.models import StockEntry
-from utils.user_serializer import UserSerializer
-from product.serializers import ProductSerializer
-from warehouse.serializers import WareHouseSerializer
+from utils.user_serializer import UserSerializer, User
+from product.serializers import ProductSerializer, Product
+from warehouse.serializers import WareHouseSerializer, WareHouse
 
 
 class StockEntrySerializer(serializers.ModelSerializer):
@@ -12,6 +13,27 @@ class StockEntrySerializer(serializers.ModelSerializer):
     warehouse = WareHouseSerializer(read_only=True)
     product = ProductSerializer(read_only=True)
     received_by = UserSerializer(read_only=True)
+
+    def validate_product_id(self, value):
+        if value is None:
+            return value
+        if not Product.objects.filter(id=value).exists():
+            raise ValidationError("Related Product does not exist.")
+        return value
+
+    def validate_warehouse_id(self, value):
+        if value is None:
+            return value
+        if not WareHouse.objects.filter(id=value).exists():
+            raise ValidationError("Related warehouse does not exist.")
+        return value
+
+    def validate_received_by_id(self, value):
+        if value is None:
+            return value
+        if not User.objects.filter(id=value).exists():
+            raise ValidationError("Related user does not exist.")
+        return value
 
     class Meta:
         model = StockEntry
