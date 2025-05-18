0f05a92fceeef672dff2a1f91f3239721de49634
ghazalmohseni7 <ghazalmohseni79@gmail.com>
2025-05-18 18:42:54 +0330

    fix: inventory just need GET api

--- inventory/views.py
+++ inventory/views.py
@@ -1,8 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
 from inventory.models import Inventory
 from inventory.serializers import InventorySerializer
+
+
 # Create your views here.
 
 class InventoryViewSets(ModelViewSet):
+    http_method_names = ['get']
     queryset = Inventory.objects.select_related('product').select_related('warehouse').all()
     serializer_class = InventorySerializer
