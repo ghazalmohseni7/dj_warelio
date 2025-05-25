89c83b034feafefa313a2ca9ee77430697016bb4
ghazalmohseni7 <ghazalmohseni79@gmail.com>
2025-05-25 14:53:48 +0330

    feat: category permission

--- category/views.py
+++ category/views.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from category.models import Category
 from category.serializers import CategorySerializer
+from category.permissions import CategoryPermissions
 
 
 # Create your views here.
@@ -8,3 +10,4 @@ from category.serializers import CategorySerializer
 class CategoryViewSets(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated, CategoryPermissions]
