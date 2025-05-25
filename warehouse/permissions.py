056afd364f8a86bd9ca7e4e85cfc4a765e8480b2
ghazalmohseni7 <ghazalmohseni79@gmail.com>
2025-05-25 15:05:41 +0330

    feat: warehouse permission

---
+++ warehouse/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class WareHousePermissions(BasePermission):
+    def has_permission(self, request, view) -> bool:
+        if not request.user.is_active:
+            return False
+        if request.method in SAFE_METHODS:
+            return True
+        return bool(request.user.is_superuser or request.user.is_staff)
--- warehouse/views.py
+++ warehouse/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from warehouse.models import WareHouse
 from warehouse.serializers import WareHouseSerializer
 from project.models import Project
+from warehouse.permissions import WareHousePermissions
 
 
 # Create your views here.
 class WareHouseViewSets(ModelViewSet):
     queryset = WareHouse.objects.select_related('project').select_related('manager').all()
     serializer_class = WareHouseSerializer
+    permission_classes = [IsAuthenticated, WareHousePermissions]
 
     def create(self, request, *args, **kwargs):
         project_id = request.data.get('project_id')
