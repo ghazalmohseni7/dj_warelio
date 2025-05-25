50ddac7d63ef600398deae0d3790bd4c98e0b827
ghazalmohseni7 <ghazalmohseni79@gmail.com>
2025-05-25 15:04:44 +0330

    feat: supplier permission

--- supplier/views.py
+++ supplier/views.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from supplier.models import Supplier
 from supplier.serializers import SupplierSerializer
+from supplier.permissions import SupplierPermissions
 
 
 # Create your views here.
@@ -8,3 +10,4 @@ from supplier.serializers import SupplierSerializer
 class SupplierViewSets(ModelViewSet):
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializer
+    permission_classes = [IsAuthenticated, SupplierPermissions]
