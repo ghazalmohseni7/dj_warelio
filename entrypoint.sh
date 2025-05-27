a64291704f75abd14e65757e89b31b5ef71d8237
ghazalmohseni7 <ghazalmohseni79@gmail.com>
2025-05-27 15:42:33 +0330

    dockerize: add rabbit healthcheck with version of ocker-compose 1.29.2

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,8 +23,10 @@ python manage.py collectstatic --noinput
 
 # Run Django, pubsub scripts in the background
 echo "Starting services..."
-/Warelio/bashes/start_django.sh &
 /Warelio/bashes/start_pubsub_subscriber.sh &
+wait
+/Warelio/bashes/start_django.sh &
+
 
 # Keep container running
 wait
